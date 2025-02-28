a08f99bb0985e2204eab26a6091286ba716be18c
Hao Nghiem <qtvhao@gmail.com>
2025-02-28 16:00:42 +0700

    Add

--- app/src/components/AppFooter.js
+++ app/src/components/AppFooter.js
@@ -1,7 +1,7 @@
 function AppFooter() {
     return (
       <footer style={{ textAlign: "center", padding: "10px", fontSize: "12px" }}>
-        Build Version: {process.env.CF_PAGES_COMMIT_SHA || "Unknown"}
+        Build Version: {process.env.REACT_APP_CF_PAGES_COMMIT_SHA || "Unknown"}
       </footer>
     );
   }
