9c9a981f56472160cfa266da8c7b8cfb19fcd1b2
Hao Nghiem <qtvhao@gmail.com>
2025-02-28 14:02:49 +0700

    Add

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import ChatBox from "./ChatBox";
+import ChatBox from "./ChatBox";
 
 function App() {
   return (
     <div className="App">
       <h2>Simple Chat Box</h2>
-      {/* <ChatBox /> */}
+      <ChatBox />
     </div>
   );
 }
